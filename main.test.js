55f697fd68da27117d22e0906332a24345f0034e
Herzallah Abdallah <32783064+abdallaho98@users.noreply.github.com>
2021-03-15 19:56:16 +0100

    Update main.test.js

--- main.test.js
+++ main.test.js
@@ -1,5 +1,5 @@
 const generateRandomBadStatusCode = require("./main")
 
 test('returns an integer', ()=> {
-    expect(generateRandomBadStatusCode(0.5)).toBeNumber();
+    expect(generateRandomBadStatusCode(0.9)).toBeNumber();
 });
